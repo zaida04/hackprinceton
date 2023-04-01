ea8e7feb85a27d95152dd26469d5d67b7e9d6852
Zaid (Nico) <nico.03727+github@gmail.com>
2023-03-31 21:17:00 -0400

    chore: tidy up index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,32 +7,10 @@ import {
   getStreamHTTP,
 } from "../lib/api-frontend";
 import Layout from "../components/Layout";
+import Link from "next/link";
 
 // Page that creators will see
 export default function Home() {
-  // stream name
-  const [name, setName] = useState("");
-
-  // created stream data, populated when the button is clicked
-  const [streamCreatorData, setStreamCreatorData] =
-    useState(null);
-
-  // stream data for playback, not the created stream data.
-  // this is the data that a viewer will rz
-  const [streamData, setStreamData] = useState(null);
-
-  // callback for stream create button
-  const onCreateStreamClick = async () => {
-    if (name == "") return;
-    // create the stream
-    const newStream = await createStreamHTTP(name);
-    setStreamCreatorData(newStream);
-
-    // using the created stream data, get the playback URL
-    const retrieveStreamData = await getStreamHTTP(newStream.id);
-    setStreamData(retrieveStreamData);
-  };
-
   return (
     <Layout>
       <main>
@@ -52,12 +30,14 @@ export default function Home() {
                     <div className="sm:mx-auto sm:max-w-xl lg:mx-0">
                       <div className="sm:flex">
                         <div className="mt-3">
+                          <Link href="/create">
                           <button
                             type="submit"
                             className="block w-full rounded-md bg-indigo-500 py-3 px-4 font-medium text-white shadow hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-300 focus:ring-offset-2 focus:ring-offset-gray-900"
                           >
                             Create a stream
                           </button>
+                          </Link>
                         </div>
                       </div>
                     </div>
@@ -78,40 +58,6 @@ export default function Home() {
           </div>
         </div>
       </main>
-
-      {/* <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
-        <div>
-          {streamData !== null && (
-            <div>
-              <VideoStream stream={streamData} />
-              <p>Stream ID: {streamCreatorData.id}</p>
-              <p>Stream Name: {streamCreatorData.name}</p>
-              <p>
-                Stream Created At:{" "}
-                {streamCreatorData.createdAt.toLocaleString()}
-              </p>
-              <p>
-                Stream Connection URL: {streamCreatorData.connectionInfo.url}
-              </p>
-              <p>
-                Stream connection token:{" "}
-                {streamCreatorData.connectionInfo.streamKey}
-              </p>
-            </div>
-          )}
-          <input
-            onChange={(e) => setName(e.target.value)}
-            placeholder="stream name here!"
-            className="p-4 border-[1px] border-gray-400 mr-2"
-          ></input>
-          <button
-            onClick={onCreateStreamClick}
-            className="bg-gray-800 rounded-lg border-[1px] border-gray-400 p-4 text-white font-bold"
-          >
-            Create Stream
-          </button>
-        </div>
-      </div> */}
     </Layout>
   );
 }
