086c9fce488170dc256de648832a97a5ac106c78
Ellis <ellispinsky5@gmail.com>
2023-03-31 23:23:18 -0400

    flashcard patch

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ReactCardFlip from "react-card-flip";
 
 const Flashcard = ({ front, back }) => {
   const [isFlipped, setIsFlipped] = useState(false);
@@ -8,27 +9,21 @@ const Flashcard = ({ front, back }) => {
   };
 
   return (
-    <div
-      onClick={handleFlip}
-      className="w-full max-w-xs p-5 m-4 bg-white border border-gray-200 rounded-lg shadow-md cursor-pointer transform transition duration-300 ease-in-out hover:scale-105"
-    >
-      <div className="relative w-full h-48">
-        <div
-          className={`absolute inset-0 flex items-center justify-center transition-transform duration-500 ease-in-out transform ${
-            isFlipped ? "rotate-y-180" : "rotate-y-0"
-          }`}
-        >
-          <div className="text-xl font-semibold text-gray-700">{front}</div>
-        </div>
-        <div
-          className={`absolute inset-0 flex items-center justify-center transition-transform duration-500 ease-in-out transform ${
-            isFlipped ? "rotate-y-0" : "rotate-y-180"
-          }`}
-        >
-          <div className="text-xl font-semibold text-gray-700">{back}</div>
-        </div>
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      <div
+        onClick={handleFlip}
+        className="w-full max-w-xs p-5 m-4 bg-white border border-gray-200 rounded-lg shadow-md cursor-pointer transform transition duration-300 ease-in-out hover:scale-105"
+      >
+        <div className="text-xl font-semibold text-gray-700">{front}</div>
       </div>
-    </div>
+
+      <div
+        onClick={handleFlip}
+        className="w-full max-w-xs p-5 m-4 bg-white border border-gray-200 rounded-lg shadow-md cursor-pointer transform transition duration-300 ease-in-out hover:scale-105"
+      >
+        <div className="text-xl font-semibold text-gray-700">{back}</div>
+      </div>
+    </ReactCardFlip>
   );
 };
 
