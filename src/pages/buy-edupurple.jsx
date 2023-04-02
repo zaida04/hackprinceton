fe3833536623e61e71f4d5e33669ad5808c8cb9f
Zaid (Nico) <nico.03727+github@gmail.com>
2023-04-02 07:43:12 -0400

    chore: add payment disclaimer

--- src/pages/buy-edupurple.jsx
+++ src/pages/buy-edupurple.jsx
@@ -6,9 +6,9 @@ export default function BuyEduPurple() {
   return (
     <Layout>
       <div className="bg-gray-900  flex flex-col justify-center text-white w-full items-center h-screen">
-        <h1 className="text-5xl font-bold py-2">Buy EduPurple</h1>
-        <p className="text-3xl">Only $8/month</p>
-        <p className="text-xl py-4">Why? IDK, shiny checkmark</p>
+        <h1 className="text-5xl font-bold py-4">Buy EduPurple</h1>
+        <p className="text-3xl">Only $8/month (not actually, just click buy)</p>
+        <p className="text-xl py-8">Why? IDK, shiny checkmark</p>
         <button
           className="py-4 px-8 bg-indigo-600 hover:bg-indigo-800 text-white rounded-lg shadow-md shadow-indigo-800"
           onClick={() => router.push("/edu-purple-payment")}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -129,7 +129,7 @@ export default function Home() {
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                     onClick={(e) => proceedCheckout(e, event.id)}
                   >
-                    Pay {event.price}
+                    Enroll {event.price}
                   </button>
                 </div>
               </div>
